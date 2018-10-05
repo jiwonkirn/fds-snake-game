4ec7f092f2ccadba1dcd3c179a1db00fe4e2567e
Kimjiwon <jhd1925@gmail.com>
2018-10-05 21:54:33 +0900

    뱀게임 방향, 종료, 먹이

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -13,49 +13,99 @@ function SnakeGameLogic() {
     {x: 0, y: 0},
   ];
 
+  this.memory = 0;
+
   // 먹이의 좌표
   this.fruit = {x: 3, y: 5};
 }
 
 SnakeGameLogic.prototype.up = function() {
-  return this.joints.reduce((acc, item) => item.y -= 1, -Infinity)
+
+  
+
   // 위쪽 화살표 키를 누르면 실행되는 함수
-  console.log('up');
+  let tail
+  this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y ? tail = {} : tail = this.joints.pop()
+
+  tail.x = this.joints[0].x
+  tail.y = this.joints[0].y - 1
+  this.joints.unshift(tail)
+
+  this.memory = 'top'
+  console.log('top');
 }
 
 SnakeGameLogic.prototype.down = function() {
+  // this.memory = 2
+
   // 아래쪽 화살표 키를 누르면 실행되는 함수
-  this.joints[this.joints.length-1].y = this.joints[0].y+1 
-  this.joints[this.joints.length-1].x = this.joints[0].x
-  this.joints.unshift(this.joints[this.joints.length-1])
-  this.joints.pop()
-  // return this.joints.reduce((acc, item) => item.y += 1, -Infinity)
+  let tail
+  this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y ? tail = {} : tail = this.joints.pop()
+
+  tail.x = this.joints[0].x
+  tail.y = this.joints[0].y + 1
+  this.joints.unshift(tail)
+
+  this.memory = 'down'
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
+  let tail
+  this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y ? tail = {} : tail = this.joints.pop()
+
+  tail.x = this.joints[0].x - 1
+  tail.y = this.joints[0].y
+  this.joints.unshift(tail)
+
+  this.memory = 'left'
   console.log('left');
 }
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
-  this.joints[this.joints.length-1].x = this.joints[0].x+1
-  this.joints[this.joints.length-1].y = this.joints[0].y 
-  this.joints.unshift(this.joints[this.joints.length-1])
-  this.joints.pop();
-  // joints[4].x+=5;
-  // joints.unshift(joints[4])
-  // joints.pop()
+  let tail
+  this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y ? tail = {} : tail = this.joints.pop()
+
+  tail.x = this.joints[0].x + 1
+  tail.y = this.joints[0].y 
+  this.joints.unshift(tail)
+
+  this.memory = 'right'
   console.log('right');
 }
 
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
-  // 게임이 아직 끝나지 않았으면 `true`를 반환
-  // 게임이 끝났으면 `false`를 반환
+  // if (this.memory === 'right') {
+  //   this.joints[this.joints.length-1].x += 1
+  // } else if (this.memory === 'left') {
+  //   for(let item of this.joints) {
+  //     item.x -= 1
+  //   }
+  // } else if (this.memory === 'top') {
+  //   for(let item of this.joints) {
+  //     item.y -= 1
+  //   }
+  // } else if (this.memory === 'bottom') {
+  //   for(let item of this.joints) {
+  //     item.y += 1
+  //   }
+  // }
+
   console.log(`nextState`);
-  return true;
+  if(this.joints[0].x < 0 || this.joints[0].y < 0 || this.joints[0].y > 19 || this.joints[0].x > 29) {
+    // 게임이 끝났으면 `false`를 반환
+    return false
+  } else {
+    // 게임이 아직 끝나지 않았으면 `true`를 반환
+    return true
+  }
 }
+
+// if (memory === 1) {
+//   return SnakeGameLogic.prototype.right()
+// }
 
 export default SnakeGameLogic;
